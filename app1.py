d6d9e59df14e4ca1bc870f3e4b8b4acbc4649274
JEEL6352 <163297822+JEEL6352@users.noreply.github.com>
2024-03-27 17:39:28 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -37,7 +37,7 @@ def set_bg_hack_url():
           f"""
           <style>
           .stApp {{
-              background: url("https://camo.githubusercontent.com/26dd456211c8565656342810c4a76d69338a6a0c61917d231c9004efe8be201c/68747470733a2f2f7777772e6f70656e6465692e65752f77702d636f6e74656e742f75706c6f6164732f323032302f31312f696d672d59616e65776e304f52574378344a6c6d2d773830302e6a7067");
+              background: url("https://removal.ai/wp-content/uploads/2021/09/black-background-03-unsplash.png");
               background-size: cover
           }}
           </style>
